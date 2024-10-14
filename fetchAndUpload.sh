2192d3078a8278fb6f2684dae34cf44ab489e598
mitespsoc <mitespsoc@gmail.com>
2024-10-14 06:24:36 +0000

    Updated IP Feed - Mon Oct 14 06:24:36 AM UTC 2024

--- fetchAndUpload.sh
+++ fetchAndUpload.sh
@@ -1,38 +1,8 @@
-#!/usr/bin/python3
-import requests
-import json
-import csv
+date=$(date)
 
-searchUrl = "http://4.194.176.219:64298/ipfeed/_search?size=10000&pretty=true"
-headers = {
-	'Content-Type': 'application/json',
-}
-query = {
-	"query":
-		{"bool":
-			{"must":[
-				{"range":{"LastDate": {"gte": "now-1d"}}}
-			]
-		}
-	}
-}
+source .venv/bin/activate
+python fetchFeed.py
 
-hits = requests.post(searchUrl, json=query, headers=headers).json()['hits']['hits']
-outFile = open('ipfeed.json', 'w')
-for hit in hits:
-	json.dump(hit['_source'], outFile)
-	outFile.write("\n")
-outFile.close()
-
-headers = ["UUID","IP","Count","Sources","FirstDate","LastDate"]
-jsonFile = open('ipfeed.json', 'r')
-ipFeedCsv = open('ipFeed.csv', 'w')
-csvWriter = csv.writer(ipFeedCsv)
-csvWriter.writerow(headers)
-for row in jsonFile:
-	ipObj = json.loads(row)
-	ipObjSrcs = ipObj['Sources'].replace(",", "-").replace(' ', "")
-	csvRow = [ipObj['UUID'],ipObj['IP'],str(ipObj['Count']),str(ipObjSrcs),ipObj['FirstDate'],ipObj['LastDate']]            
-	csvWriter.writerow(csvRow)
-jsonFile.close()
-ipFeedCsv.close()
+git add ipFeed.csv ipfeed.json
+git commit -m "Updated IP Feed - $date"
+git push
